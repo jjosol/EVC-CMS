faf7db1751c633da69bfb833ff2df6c98adcf1ef
Seb0ng <98243994+Seb0ng@users.noreply.github.com>
2024-07-11 08:32:35 -0400

    Frontend (#2)
    * Initial Commit
    
    BASE Environemtn
    
    * Login page (not done yet)
    
    * Login Page initial
    
    ---------
    
    Co-authored-by: John Josol <97109454+jjosol@users.noreply.github.com>

---
+++ CMS/src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import pisayLogo from '../components/icons/IconSupport.vue'
+</script>
+<template>
+<div class="items-center justify-center hidden lg:flex ">
+    <div class="flex items-center">
+    <pisayLogo class="-mr-5"/>
+    <div>
+        <h1 class="text-2xl font-bold text-[#2F4A71]">Philippine Science High School</h1>
+        <h3 class="text-xl text-[#2F4A71]">Eastern Visayas Campus</h3>
+    </div>
+    </div>
+</div>
+</template>
---
+++ CMS/src/components/icons/IconSupport.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="../../assets/Logo.svg" width="150" height="150" />
+</template>
